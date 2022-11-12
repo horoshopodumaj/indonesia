3a9f5387c6deafa5b993c6cf5f8ffad7193a3c2e
Diana Murtazina <murka8831@yandex.ru>
2022-11-12 19:50:58 +0400

    added code for open and close burger-menu

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,13 @@ new Gallery(document.getElementById("gallery"), {
     margin: 10,
     dots: false,
 });
+
+/*Burger*/
+
+const MENU_OPENED_CLASS = "burger-menu_opened";
+const burgerNode = document.querySelector(".burger-menu");
+const burgerControlNode = document.querySelector(".burger-menu__control");
+
+burgerControlNode.addEventListener("click", (_event) => {
+    burgerNode.classList.toggle(MENU_OPENED_CLASS);
+});

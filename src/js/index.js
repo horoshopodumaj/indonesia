f12e6f6d9ba967fea2ec7294f8a608180f2e4b3e
Diana Murtazina <murka8831@yandex.ru>
2022-11-14 23:45:54 +0400

    added scroll down

--- src/js/index.js
+++ src/js/index.js
@@ -39,3 +39,37 @@ function toggleBgHeader() {
         headerNode.classList.remove(HEADER_SCROLLED_CLASS);
     }
 }
+
+/* Scroll Down */
+
+const scrollDownButton = document.getElementById("scroll-action");
+const scrollToNode = document.getElementById("scroll-to");
+
+let currentScroll = window.scrollY;
+let scrollAnimationId;
+
+scrollDownButton.addEventListener("click", (evt) => {
+    stopAnimationScroll();
+
+    currentScroll = window.scrollY;
+    startAnimationScroll(scrollToNode.offsetTop - 80);
+});
+
+function startAnimationScroll(newScrollY) {
+    const deltaScroll = newScrollY - currentScroll;
+
+    currentScroll += deltaScroll * 0.08;
+    window.scrollTo(0, currentScroll);
+
+    if (Math.abs(deltaScroll) > 1) {
+        scrollAnimationId = window.requestAnimationFrame(() => startAnimationScroll(newScrollY));
+    } else {
+        window.scrollTo(0, newScrollY);
+        stopAnimationScroll();
+    }
+}
+
+function stopAnimationScroll() {
+    window.cancelAnimationFrame(scrollAnimationId);
+    scrollAnimationId = undefined;
+}
